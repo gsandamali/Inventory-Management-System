718da44930be6a41ba9d546412bfc0b7d2da1fbf
Geethika Sandamali <86560608+gsandamali@users.noreply.github.com>
2022-01-16 17:38:43 +0530

    Update app.ts
    change to create separate routes

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { saveMaterial, findMaterialByName } from './service/material.service';
+import { apiRoutes } from './routes/api';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-// const router = express.Router();
-const port = 3000;
-// const contextPath = '/api';
-const mongoUri: string = "local url";
+const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
 
 // app.use(contextPath, router)
 app.use(express.json());
-app.use(express.static('api'));
 
 app.get('/', (req, res) => {
   res.send("server is running.");
 });
 
-app.post('/materials/new', (req, res) => {
-  saveMaterial({name: req.body.name, type: req.body.type}).then(() => {
-    res.send("saved successfully!");
-  }).catch(err => {
-    res.send(err.getMessage());
-  });
-});
-
-app.get('/materials/:name', (req, res) => {
-  var materialName = req.params.name;
-  console.log(materialName);
-  findMaterialByName(materialName).then(material => {
-    if(material) {
-      res.send({status: "success", message: "material found", payload: {name: material.name, type: material.type}});
-    } else {
-      res.send({status: "failed", message: "no such data", payload: null});
-    }
-  }).catch(err => {
-    res.send({status: "failed", message: "error occured", payload: err.getMessage()});
-  });
-});
+app.use('/api', apiRoutes);
 
 app.listen(port, () => {
   mongoose.connect(mongoUri);
