d282bdcf2fb48b104c9d3aa3a1c598ea85575271
gsandamali <gsandamali30@gmail.com>
2022-01-17 22:03:20 +0530

    change material inventory details

--- src/models/material.model.ts
+++ src/models/material.model.ts
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 import { IMaterial } from '../interfaces/IMaterial';
 
 const MaterialSchema = new mongoose.Schema({
-  productName: String,
-  amount: Number,
+  product: String,
+  quantity: Number,
   unit: String,
-  direction: String
+  direction: String,
+  updated: { type: Date, default: Date.now() },
 });
 
 let MaterialModel = mongoose.model<IMaterial>('material', MaterialSchema);
