3390bddf03c1f80bf4870b618d924524e0ee8860
gsandamali <gsandamali30@gmail.com>
2022-01-17 22:07:37 +0530

    change material inventory details

--- src/utils/material.mapper.ts
+++ src/utils/material.mapper.ts
@@ -3,8 +3,8 @@ import { IMaterial } from '../interfaces/IMaterial';
 
 function mapToMaterial(req: Request) {
     const material = {} as IMaterial;
-    material.productName = req.body.name;
-    material.amount = req.body.amount;
+    material.product = req.body.product;
+    material.quantity = req.body.quantity;
     material.unit = req.body.unit;
     material.direction = req.body.direction;
 
