59e5f748229948b2711b2a55e0f85c4357521e44
gsandamali <gsandamali30@gmail.com>
2022-01-17 11:32:44 +0530

    Add file to save and find materials

---
+++ src/service/material.service.ts
@@ -0,0 +1,15 @@
+import { MaterialModel } from '../models/material.model';
+import { IMaterial} from '../interfaces/IMaterial';
+
+async function saveMaterial(material: IMaterial) {
+    var materialToSave = new MaterialModel(material);
+    materialToSave.save();
+}
+
+async function findMaterialByName(materialName: string) {
+    var material = await MaterialModel.findOne({name: materialName});
+    console.log(material);
+    return material;
+}
+
+export { saveMaterial, findMaterialByName };
